2e0b3a40c2f6346b56ae9e28769a1bcacee31382
Nguyễn Khánh <108068667+independenceee@users.noreply.github.com>
2024-10-07 21:24:15 +0700

    add all the necessary components to the project and config prettierc

---
+++ src/app/(home)/page.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+import { cn } from "@/utils";
+
+type Props = {};
+
+const Home = function ({}: Props) {
+    return (
+        <main className={cn("bg-[#282c34]")}>
+            {/* banner-begin */}
+            <section className={cn("")}></section>
+            {/* banner-end */}
+
+            {/* about-begin */}
+            <section className={cn("")}></section>
+            {/* about-end */}
+
+            {/* feature-begin */}
+            <section className={cn("")}></section>
+            {/* feature-end */}
+
+            {/* founder-begin */}
+            <section className={cn("")}></section>
+            {/* founder-end */}
+        </main>
+    );
+};
+
+export default Home;
