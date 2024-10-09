954aa01095c75b2c1d30dd951a0e17977c3a8e5c
tidvn <tiendung0325@gmail.com>
2024-10-09 09:29:06 +0700

    fix bug main layout

--- src/components/layouts/desktop-dashboard/index.tsx
+++ src/components/layouts/desktop-dashboard/index.tsx
@@ -1,15 +1,27 @@
 'use client';
-import { Navbar } from './navbar';
+
+import { cn } from '@/utils';
+import { useSidebarToggle } from '@/hooks/use-sidebar-toggle';
 import { Sidebar } from './sidebar';
+import { Navbar } from './navbar';
 
 export default function DesktopDashboardlLayout({ children }: { children: React.ReactNode }) {
+    const sidebar = useSidebarToggle();
+
+    if (!sidebar) return null;
+
     return (
-        <div className="relative overflow-hidden">
+        <>
             <Sidebar />
-            <div className="relative flex h-full min-w-0 flex-1 flex-col">
+            <main
+                className={cn(
+                    'min-h-[calc(100vh_-_56px)] transition-[margin-left] duration-300 ease-in-out dark:bg-zinc-900',
+                    sidebar?.isOpen === false ? 'lg:ml-[90px]' : 'lg:ml-[300px]',
+                )}
+            >
                 <Navbar />
-                <div className="flex-1 overflow-y-auto overflow-x-hidden">{children}</div>
-            </div>
-        </div>
+                <div className="container mt-10 px-4 pb-16 pt-8 sm:px-8">{children}</div>
+            </main>
+        </>
     );
 }
--- src/components/layouts/desktop-dashboard/navbar.tsx
+++ src/components/layouts/desktop-dashboard/navbar.tsx
@@ -1,13 +1,17 @@
+import { Images } from '@/components/common/images';
 import { WalletConnectButton } from '@/components/common/mesh/wallet-connect';
 
 export function Navbar() {
     return (
-        <header className="sticky left-0 right-0 top-0 z-10 h-full max-h-16 flex-1 bg-section">
-            <nav className="flex items-center justify-between px-4 py-2 md:justify-end">
-                <div className="flex items-center gap-2">
+        <header className="fixed right-0 top-0 z-10 w-full bg-section">
+            <div className="mx-4 flex h-14 items-center sm:mx-8">
+                <div className="flex flex-1 items-center justify-start md:hidden">
+                    <Images.logo className="h-4 w-16" />
+                </div>
+                <div className="flex flex-1 items-center justify-end space-x-2">
                     <WalletConnectButton />
                 </div>
-            </nav>
+            </div>
         </header>
     );
 }
