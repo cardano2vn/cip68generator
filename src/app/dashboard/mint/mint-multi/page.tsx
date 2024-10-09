57188e496ac7e8ddb5a439039966e59019bf2937
tidvn <tiendung0325@gmail.com>
2024-10-09 22:13:16 +0700

    add page

--- src/app/dashboard/mint/mint-multi/page.tsx
+++ src/app/dashboard/mint/mint-multi/page.tsx
@@ -4,7 +4,7 @@ export default function Page() {
     return (
         <>
             <div className="h-[500px] w-full bg-section">
-                <Card className="h-[200px] w-[200px]">hvkjb</Card>
+                <Card className="h-[200px] w-[200px]">mint page</Card>
             </div>
         </>
     );
--- src/app/dashboard/mint/page.tsx
+++ src/app/dashboard/mint/page.tsx
@@ -3,8 +3,8 @@ import { Card } from '@/components/ui/card';
 export default function Page() {
     return (
         <>
-            <div className="h-[5000px] w-full bg-section">
-                <Card className="h-[200px] w-[200px]">hvkjb</Card>
+            <div className="h-[500px] w-full bg-section">
+                <Card className="h-[200px] w-[200px]">mint page</Card>
             </div>
         </>
     );
