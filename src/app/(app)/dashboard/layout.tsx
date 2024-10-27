e9423569882835d7475fdb5a6a670dbfd610c0a1
tidvn <tiendung0325@gmail.com>
2024-10-28 04:08:56 +0700

    update layout

--- src/app/(app)/dashboard/layout.tsx
+++ src/app/(app)/dashboard/layout.tsx
@@ -1,14 +1,18 @@
 "use client";
 
-import DesktopDashboardlLayout from "@/components/layouts/desktop-dashboard";
-import MobileDashboardlLayout from "@/components/layouts/mobile-dashboard";
+import DesktopDashboardLayout from "@/components/layouts/desktop-dashboard";
+import MobileDashboardLayout from "@/components/layouts/mobile-dashboard";
 import useWindowSize from "@/hooks/use-window-size";
-import { PropsWithChildren } from "react";
+import { PropsWithChildren, useMemo } from "react";
 
 export default function DashboardLayout({
   children,
 }: Readonly<PropsWithChildren>) {
   const isMobile: boolean = useWindowSize();
-  const Layout = isMobile ? MobileDashboardlLayout : DesktopDashboardlLayout;
+
+  const Layout = useMemo(() => {
+    return isMobile ? MobileDashboardLayout : DesktopDashboardLayout;
+  }, [isMobile]);
+
   return <Layout>{children}</Layout>;
 }
