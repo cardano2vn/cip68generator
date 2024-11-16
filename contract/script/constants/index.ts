d8d449f0b632191674663994e935b3a8e0a769cf
tidvn <tiendung0325@gmail.com>
2024-11-16 21:10:30 +0700

    feat: update compiler version and refactor reference script handling in CIP68 contract

--- contract/script/constants/index.ts
+++ contract/script/constants/index.ts
@@ -1,15 +1,12 @@
 export const EXCHANGE_FEE_ADDRESS = process.env.EXCHANGE_FEE_ADDRESS || "";
-export const MINT_REFERENCE_SCRIPT_ADDRESS =
-  process.env.MINT_REFERENCE_SCRIPT_ADDRESS || "";
-export const STORE_REFERENCE_SCRIPT_ADDRESS =
-  process.env.STORE_REFERENCE_SCRIPT_ADDRESS || "";
-export const MINT_REFERENCE_SCRIPT_HASH =
-  process.env.MINT_REFERENCE_SCRIPT_HASH || "";
-export const STORE_REFERENCE_SCRIPT_HASH =
-  process.env.STORE_REFERENCE_SCRIPT_HASH || "";
+export const REFERENCE_SCRIPT_ADDRESS =
+  process.env.REFERENCE_SCRIPT_ADDRESS || "";
+
+export const REFERENCE_SCRIPT_HASH = process.env.REFERENCE_SCRIPT_HASH || "";
+
 export const EXCHANGE_FEE_PRICE = process.env.EXCHANGE_FEE_PRICE || "";
 
 export const title = {
-  mint: "mint.mint.mint",
-  store: "store.store.spend",
+  mint: "cip68generator.cip68generator.mint",
+  store: "cip68generator.cip68generator.spend",
 };
