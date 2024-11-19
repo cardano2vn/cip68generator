374cfa59f1c6f83c5e72328556e2be07837d50b1
tidvn <tiendung0325@gmail.com>
2024-11-19 18:53:42 +0700

    fix: remove unnecessary whitespace in ProfilePage component

--- src/app/(app)/dashboard/(profile)/page.tsx
+++ src/app/(app)/dashboard/(profile)/page.tsx
@@ -35,8 +35,7 @@ export default function ProfilePage() {
             Bulk list
           </Button>
           <Button className="shrink-0">Create Collection</Button>
-        </div>{" "}
-        *
+        </div>
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 gap-4">
           {listNft.map((data, index) => (
             <AssetCard data={data} key={index} />
